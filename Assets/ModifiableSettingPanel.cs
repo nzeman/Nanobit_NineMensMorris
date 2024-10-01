64d3e663d631318d097e79f53f1497cbae9dd62c
nzeman <nikola.zeman1337@gmail.com>
2024-10-01 10:36:53 +0200

    Limit the number of rings, calculate the minimum and maximum number of pieces per player

--- Assets/ModifiableSettingPanel.cs
+++ Assets/ModifiableSettingPanel.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,60 +13,94 @@ public class ModifiableSettingPanel : MonoBehaviour
     [SerializeField] private Button deductOneButton;
     [SerializeField] private TMP_Text amountText;
 
-
     public int currentAmount = 0;
     public int minAmount = 0;
     public int maxAmount = 0;
 
-    // Start is called before the first frame update
+    [SerializeField] private ModifiableSettingPanel piecesPerPlayerPanel; 
+
     void Start()
     {
-        plusOneButton.onClick.AddListener(() =>
-        {
-            ModifyAmount(1);
-        });
-
-        deductOneButton.onClick.AddListener(() =>
-        {
-            ModifyAmount(-1);
-        });
+        plusOneButton.onClick.AddListener(() => ModifyAmount(1));
+        deductOneButton.onClick.AddListener(() => ModifyAmount(-1));
 
         if (setting == ModifiableSetting.NumberOfRings)
         {
             currentAmount = PlayerProfile.Instance.playerData.gameRulesData.numberOfRings;
+            UpdateMaxPiecesPerPlayer(); 
         }
         else
         {
             currentAmount = PlayerProfile.Instance.playerData.gameRulesData.numberOfPiecesPerPlayer;
         }
-
-        ModifyAmount(0);
+        ModifyAmount(0); // Initial update
     }
 
     public void ModifyAmount(int amount)
     {
         currentAmount += amount;
-        if (amount < 0)
-        {
-            if (currentAmount <= minAmount)
-            {
-                currentAmount = minAmount;
-            }
-        }
-        else
-        {
-            if (currentAmount >= maxAmount)
-            {
-                currentAmount = maxAmount;
-            }
-        }
+        currentAmount = Mathf.Clamp(currentAmount, minAmount, maxAmount);
         amountText.text = currentAmount.ToString();
         ApplyValues();
+        UpdateButtonInteractability();
+
+        if (setting == ModifiableSetting.NumberOfRings)
+        {
+            UpdateMaxPiecesPerPlayer();
+        }
+    }
+
+    private void UpdateButtonInteractability()
+    {
+        plusOneButton.interactable = currentAmount < maxAmount;
+        UpdateButtonAlpha(plusOneButton, plusOneButton.interactable);
+
+        deductOneButton.interactable = currentAmount > minAmount;
+        UpdateButtonAlpha(deductOneButton, deductOneButton.interactable);
+    }
+
+    private void UpdateButtonAlpha(Button button, bool interactable)
+    {
+        TMP_Text buttonText = button.GetComponentInChildren<TMP_Text>();
+
+        if (buttonText != null)
+        {
+            Color textColor = buttonText.color;
+            textColor.a = interactable ? 1f : 0.1f;
+            buttonText.color = textColor;
+        }
+    }
+
+    private void UpdateMaxPiecesPerPlayer()
+    {
+        int totalPositions = CalculateTotalPositions(currentAmount);
+        int maxPiecesPerPlayer = totalPositions / 2;
+
+        if (piecesPerPlayerPanel != null)
+        {
+            piecesPerPlayerPanel.minAmount = 3; 
+            piecesPerPlayerPanel.maxAmount = maxPiecesPerPlayer; 
+            piecesPerPlayerPanel.currentAmount = Mathf.Clamp(piecesPerPlayerPanel.currentAmount, piecesPerPlayerPanel.minAmount, piecesPerPlayerPanel.maxAmount);
+            piecesPerPlayerPanel.amountText.text = piecesPerPlayerPanel.currentAmount.ToString();
+            piecesPerPlayerPanel.ApplyValues();
+            piecesPerPlayerPanel.UpdateButtonInteractability();
+        }
+        Debug.Log($"Max pieces per player: {maxPiecesPerPlayer}");
+    }
+
+    private int CalculateTotalPositions(int rings)
+    {
+        int positions = 8; // One ring has 8 positions
+        for (int i = 2; i <= rings; i++)
+        {
+            positions += 8; // Each additional ring adds 8 more positions
+        }
+        return positions;
     }
 
     public void ApplyValues()
     {
-        if(setting == ModifiableSetting.NumberOfRings)
+        if (setting == ModifiableSetting.NumberOfRings)
         {
             PlayerProfile.Instance.playerData.gameRulesData.numberOfRings = currentAmount;
         }
@@ -76,11 +109,5 @@ public class ModifiableSettingPanel : MonoBehaviour
             PlayerProfile.Instance.playerData.gameRulesData.numberOfPiecesPerPlayer = currentAmount;
         }
         PlayerProfile.Instance.SavePlayerProfile();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
