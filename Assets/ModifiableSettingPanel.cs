211c7ff45d9397b11b4e16697df39951b3790b19
nzeman <nikola.zeman1337@gmail.com>
2024-10-01 18:38:59 +0200

    Update ModifiableSettingPanel.cs

--- Assets/ModifiableSettingPanel.cs
+++ Assets/ModifiableSettingPanel.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,40 +16,46 @@ public class ModifiableSettingPanel : MonoBehaviour
     public int minAmount = 0;
     public int maxAmount = 0;
 
-    [SerializeField] private ModifiableSettingPanel numberOfRingsPanel; // Link the number of rings panel
+    [SerializeField] private ModifiableSettingPanel piecesPerPlayerPanel;
 
     void Start()
     {
         plusOneButton.onClick.AddListener(() => ModifyAmount(1));
         deductOneButton.onClick.AddListener(() => ModifyAmount(-1));
 
-        // Initialize currentAmount based on the setting type
         if (setting == ModifiableSetting.NumberOfRings)
         {
             currentAmount = PlayerProfile.Instance.playerData.gameRulesData.numberOfRings;
-            UpdateMaxPiecesPerPlayer();
+            UpdateMaxPiecesPerPlayer(currentAmount);
         }
         else if (setting == ModifiableSetting.PiecesPerPlayer)
         {
             currentAmount = PlayerProfile.Instance.playerData.gameRulesData.numberOfPiecesPerPlayer;
-            UpdateMaxPiecesPerPlayer(); // Ensure max pieces are updated
         }
 
-        ModifyAmount(0); // Initial update
+        ModifyAmount(0);
     }
 
     public void ModifyAmount(int amount)
     {
         currentAmount += amount;
-        currentAmount = Mathf.Clamp(currentAmount, minAmount, maxAmount);
+
+        if (setting == ModifiableSetting.NumberOfRings)
+        {
+            currentAmount = Mathf.Clamp(currentAmount, 1, 6);
+        }
+        else
+        {
+            currentAmount = Mathf.Clamp(currentAmount, minAmount, maxAmount);
+        }
+
         amountText.text = currentAmount.ToString();
         ApplyValues();
         UpdateButtonInteractability();
 
-        // If this is the rings panel, update the pieces per player panel based on the new rings value
-        if (setting == ModifiableSetting.NumberOfRings)
+        if (setting == ModifiableSetting.NumberOfRings && piecesPerPlayerPanel != null)
         {
-            UpdateMaxPiecesPerPlayer(); // This updates the piecesPerPlayerPanel
+            piecesPerPlayerPanel.UpdateMaxPiecesPerPlayer(currentAmount);
         }
     }
 
@@ -66,7 +71,6 @@ public class ModifiableSettingPanel : MonoBehaviour
     private void UpdateButtonAlpha(Button button, bool interactable)
     {
         TMP_Text buttonText = button.GetComponentInChildren<TMP_Text>();
-
         if (buttonText != null)
         {
             Color textColor = buttonText.color;
@@ -75,17 +79,15 @@ public class ModifiableSettingPanel : MonoBehaviour
         }
     }
 
-    private void UpdateMaxPiecesPerPlayer()
+    public void UpdateMaxPiecesPerPlayer(int numberOfRings)
     {
-        // Get the current number of rings from the rings panel
-        int numberOfRings = (numberOfRingsPanel != null) ? numberOfRingsPanel.currentAmount : 3; // Default to 3 rings if not available
         int totalPositions = CalculateTotalPositions(numberOfRings);
-        int maxPiecesPerPlayer = totalPositions / 2;
+        int maxPiecesPerPlayer = Mathf.Max(3, totalPositions / 2);
 
         if (setting == ModifiableSetting.PiecesPerPlayer)
         {
-            minAmount = 3; // Set the minimum number of pieces to 3
-            maxAmount = maxPiecesPerPlayer; // Set the maximum pieces based on the number of rings
+            minAmount = 3;
+            maxAmount = maxPiecesPerPlayer;
 
             currentAmount = Mathf.Clamp(currentAmount, minAmount, maxAmount);
             amountText.text = currentAmount.ToString();
@@ -98,10 +100,10 @@ public class ModifiableSettingPanel : MonoBehaviour
 
     private int CalculateTotalPositions(int rings)
     {
-        int positions = 8; // One ring has 8 positions
+        int positions = 8;
         for (int i = 2; i <= rings; i++)
         {
-            positions += 8; // Each additional ring adds 8 more positions
+            positions += 8;
         }
         return positions;
     }
